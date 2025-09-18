1aae63918ae5bf331603b6851754b4b75bd2d585
Tobias Wellnitz, DH1TW <tobias@dh1tw.de>
2025-09-19 00:44:42 +0200

    fixed Event Update Unit tests

--- event_test.go
+++ event_test.go
@@ -19,19 +19,19 @@ func TestState(t *testing.T) {
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, s.Keys[0], test.ShouldBeFalse)
 	test.That(t, s.Keys[1], test.ShouldBeFalse)
-	test.That(t, myEvent.String(), test.ShouldEqual, "key-released:0")
+	test.That(t, myEvent[0].String(), test.ShouldEqual, "key-released:0")
 
 	myEvent, err = s.Update(nil, []byte{1, 0, 8, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, s.Keys[0], test.ShouldBeFalse)
 	test.That(t, s.Keys[1], test.ShouldBeTrue)
-	test.That(t, myEvent.String(), test.ShouldEqual, "key-pressed:1")
+	test.That(t, myEvent[0].String(), test.ShouldEqual, "key-pressed:1")
 
 	// dial turns
 	myEvent, err = s.Update(nil, []byte{1, 3, 5, 0, 1, 2})
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, s.DialPos[0], test.ShouldEqual, 52)
-	test.That(t, myEvent.String(), test.ShouldEqual, "dial-turn:0")
+	test.That(t, myEvent[0].String(), test.ShouldEqual, "dial-turn:0")
 
 	myEvent, err = s.Update(nil, []byte{1, 3, 5, 0, 1, 1})
 	test.That(t, err, test.ShouldBeNil)
@@ -48,24 +48,24 @@ func TestState(t *testing.T) {
 	myEvent, err = s.Update(nil, []byte{1, 3, 5, 0, 1, 254})
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, s.DialPos[0], test.ShouldEqual, 97)
-	test.That(t, myEvent.String(), test.ShouldEqual, "dial-turn:0")
+	test.That(t, myEvent[0].String(), test.ShouldEqual, "dial-turn:0")
 
 	s.DialPos[0] = 0
 	myEvent, err = s.Update(nil, []byte{1, 3, 5, 0, 1, 255})
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, s.DialPos[0], test.ShouldEqual, 0)
-	test.That(t, myEvent.String(), test.ShouldEqual, "dial-turn:0")
+	test.That(t, myEvent[0].String(), test.ShouldEqual, "dial-turn:0")
 
 	// dial pushes
 	myEvent, err = s.Update(nil, []byte{1, 3, 5, 0, 0, 1})
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, s.DialPush[0], test.ShouldBeTrue)
-	test.That(t, myEvent.String(), test.ShouldEqual, "dial-pressed:0")
+	test.That(t, myEvent[0].String(), test.ShouldEqual, "dial-pressed:0")
 
 	myEvent, err = s.Update(nil, []byte{1, 3, 5, 0, 0, 0})
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, s.DialPush[0], test.ShouldBeFalse)
-	test.That(t, myEvent.String(), test.ShouldEqual, "dial-released:0")
+	test.That(t, myEvent[0].String(), test.ShouldEqual, "dial-released:0")
 
 }
 
@@ -83,25 +83,25 @@ func TestStateOriginal(t *testing.T) {
 	test.That(t, s.Keys[0], test.ShouldBeFalse)
 	test.That(t, s.Keys[1], test.ShouldBeFalse)
 	test.That(t, s.Keys[4], test.ShouldBeTrue)
-	test.That(t, myEvent.String(), test.ShouldEqual, "key-pressed:4")
+	test.That(t, myEvent[0].String(), test.ShouldEqual, "key-pressed:4")
 
 	myEvent, err = s.Update(&Original, []byte{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
 	t.Logf("myEvent 2: %v", myEvent)
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, s.Keys[0], test.ShouldBeFalse)
 	test.That(t, s.Keys[1], test.ShouldBeFalse)
-	test.That(t, myEvent.String(), test.ShouldEqual, "key-released:4")
+	test.That(t, myEvent[0].String(), test.ShouldEqual, "key-released:4")
 
 	// button 10
 	myEvent, err = s.Update(&Original, []byte{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1})
 	t.Logf("myEvent 2: %v", myEvent)
 	test.That(t, err, test.ShouldBeNil)
-	test.That(t, myEvent.String(), test.ShouldEqual, "key-pressed:10")
+	test.That(t, myEvent[0].String(), test.ShouldEqual, "key-pressed:10")
 
 	// button 14
 	myEvent, err = s.Update(&Original, []byte{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0})
 	t.Logf("myEvent 3: %v", myEvent)
 	test.That(t, err, test.ShouldBeNil)
-	test.That(t, myEvent.String(), test.ShouldEqual, "key-pressed:14")
+	test.That(t, myEvent[0].String(), test.ShouldEqual, "key-pressed:14")
 
 }
